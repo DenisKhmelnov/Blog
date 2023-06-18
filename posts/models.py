aa8328ec28e426a91915f08031ff82c877bba7c0
Denis Khmelnov <denishmelnov@gmail.com>
2023-06-18 12:59:25 +0500

    feat: create models User, Comment, Post

---
+++ posts/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, BaseModel):
+    phone = models.CharField(max_length=12)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts', null=True, blank=True)
+
+
+class Comment(BaseModel):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)

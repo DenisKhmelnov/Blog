ca5e1ea4cf2fd6dabadc1d25767a1bd0bb5de79c
Denis Khmelnov <denishmelnov@gmail.com>
2023-06-20 00:32:26 +0500

    feat: create validators for password and email

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,17 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from posts.models import User, Post, Comment
+from posts.validators import password_validator, mail_validator
 
 
 class UserSerializer(ModelSerializer):
+    password = serializers.CharField(validators=[password_validator])
+    email = serializers.CharField(validators=[mail_validator])
+
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ["phone", "birth_date", "username", "password", "email"]
 
 
 class PostSerializer(ModelSerializer):
---
+++ posts/validators.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+
+def password_validator(value:str):
+    has_digits = False
+    for symbol in value:
+        if symbol.isdigit():
+            has_digits = True
+
+    if len(value) < 8:
+        raise serializers.ValidationError("Ваш пароль слишком короткий")
+    elif not has_digits:
+        raise  serializers.ValidationError("В вашем пароле нет цифр")
+
+
+def mail_validator(value:str):
+    if  not value.endswith(('mail.ru', 'yandex.ru')):
+        raise serializers.ValidationError("Допустимо использовать только почу mail.ru и yandex.ru")
+
+

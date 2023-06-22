fafa439e6c5a054cbb607d0287873b6b96fe95f1
Denis Khmelnov <denishmelnov@gmail.com>
2023-06-22 13:22:19 +0500

    feat: create link to author from post and filter by creation date from admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from posts.models import User, Comment, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_link', 'created_at')
+    list_filter = ('created_at',)
+
+    def author_link(self, obj):
+        link = "/admin/posts/user/{}/change/".format(obj.author.id)
+        return format_html('<a href="{}">{}</a>', link, obj.author.username)
+
+    author_link.short_description = 'Автор'
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(User)
 admin.site.register(Comment)
-admin.site.register(Post)
+
+
--- posts/models.py
+++ posts/models.py
@@ -13,12 +13,20 @@ class User(AbstractUser, BaseModel):
     phone = models.CharField(max_length=12)
     birth_date = models.DateField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
 
 class Post(BaseModel):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts', null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
 
     def __str__(self):
         return self.title
@@ -28,3 +36,7 @@ class Comment(BaseModel):
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
